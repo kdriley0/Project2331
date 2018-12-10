589de0604f9e763a9b76da08c143a97bdf7b0651
kdriley0 <kdriley0@frostburg.edu>
2018-12-10 12:56:34 -0500

    as of monday morning

--- src/views/code.js
+++ src/views/code.js
@@ -1,7 +1,45 @@
 
+import { ContactSchema } from './../models/model';
+import { addNewContact } from './../controllers/controller';
 $(function(){
+    var $contacts = $('#contacts');
+    var $fname = $('fname');
+    var $lname = $('#lname');
+    var $email = $('email');
 $.ajax({
     type: 'Get',
-    url:'localhost:3000/contact'
-})
+    url:'localhost:3000/contact',
+    success: function(contacts) {
+        console.log('success', contacts);
+        $.each(contact, function(i, contact){
+            $contacts.append('<li>contact #'+i+ 'First Name:',contact.$fname+'</li>');
+           
+        });
+
+    },
+    error: function(){
+        alert('error loading contacts');
+    }
+    });
+    $('#add-contact').on('click', function(){
+        var contact ={
+            lastName = $lname.val(),
+            firstName = $fname.val(),
+        };
+        $.ajax({
+            type: 'Post',
+            url: 'http://localhost:3000/contact',
+            data: contact,
+            success: function(newContact){
+                $contacts.append('<li>contact #'+i+ 'First Name:',contact.$fname+'</li>');
+
+            },
+            error: function(){
+                alert('error saving contact');
+            }
+            
+        })
+    })
+   
+
 });
