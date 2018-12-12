fe42b175f21893bce8e9ee201fdaf1f3a38dc1da
kdriley0 <kdriley0@frostburg.edu>
2018-12-12 15:10:01 -0500

    last upload

---
+++ views/code.js
@@ -0,0 +1,61 @@
+
+
+$(function() {
+    
+    var $contacts = $('#contacts');
+    var id=$('#id')
+    var $fname = $('#fname');
+    var $lname = $('#lname');
+    var $phone = $('#phone');
+    var $company = $('#cname')
+    var $email = $('#email');
+    
+$.ajax({    
+    type: 'GET',
+    url:'/contact',
+    success: function(data) {
+        console.log('success', data);
+        $.each(data, function(i, con){
+            $contacts.append('<li>contact #'+i+ ' First Name: '+con.firstName+' Last Name: '+ con.lastName + 'Phone #:  '+con.phone + 'Company '+con.company+
+            ' email '+con.email+'<button id=del'+i+'>delete!</button>'+' <button id="change'+i+'">Change!</button> '+'</li>');
+           
+        });
+
+    },
+    error: function(){
+        alert('error loading contacts');
+    }
+    });
+    $('#add-contact').on('click', function(){
+        var contact ={
+            firstName: $fname.val(),
+            lastName: $lname.val(),
+            company: $company.val(),
+            phone: $phone.val(),
+            email: $email.val()
+        };
+        $.ajax({
+            type: 'POST',
+            url: '/contact',
+            data: contact,
+            success: function(){
+                $contacts.append('<li> First Name:'+contact.firstName+' Last Name '+ contact.lastName+ 'Company '+contact.company +' email '+contact.email +
+               'Phone # '+contact.phone  +'</li>')
+            },
+            error: function(){
+                alert('error saving contact');
+            }
+            
+        })
+        $.ajax({
+            type : 'GET',
+            url : '/contact',
+            success: function(data){
+                
+            }
+        })
+       
+
+    })
+
+});
---
+++ views/script.js
@@ -0,0 +1,3 @@
+alert("hi")
+let d = new Date();
+document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
